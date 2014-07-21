8c00728465e172d35850656a9c201197433c0c38
J.J. Idt & Brandon Madsen <student@epicodus.com>
2014-07-21 16:30:55 -0700

    This commit will run the website correctly and will alert the player if they win or lose

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ var Game = {
     this.words = ["basketball", "portland", "trapeze", "banana", "hamburger", "quiche", "target", "lawyer", "doctor", "hangman", "yellow", "bingo", "automobile", "pizza", "family", "vertigo", "island", "warrior", "lake", "skateboard"];
     this.ranWord();
     this.splitWords = this.splitWord();
+    this.lettersFound = 0;
   },
   create: function() {
     var newGame = Object.create(Game);
@@ -42,16 +43,27 @@ $(document).ready(function(){
   }
   $("form#guess").submit(function(event) {
     event.preventDefault();
-    var inputtedLetter = $("input#letter").val();
+    var inputtedLetter = $("input#letter").val().toLowerCase();
     var letterFound = false;
+
     for(var j = 0; j<hangGame.splitWords.length; j++) {
       if(inputtedLetter === hangGame.splitWords[j]) {
         $("div.display div").eq(j).removeClass("hide-text");
         letterFound = inputtedLetter;
+        hangGame.lettersFound += 1;
       }
     }
     if (letterFound === false) {
+      hangGame.man.addPart();
+    }
+    $(".hangman").css({"height": 76*hangGame.man.parts})
+    if (hangGame.hasLost()) {
+      $("div.display div").removeClass("hide-text");
+      alert("You Lose!");
 
+
+    } else if (hangGame.lettersFound === hangGame.splitWords.length) {
+      alert("You Win!");
     }
   $("input#letter").val("");
   })
